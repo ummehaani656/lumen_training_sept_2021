5311f6694399c7f33a29638b2250535fd149e31e
ummehaani656 <ummehaani656@gmail.com>
2021-10-07 19:01:58 +0530

    Req-102 the newly created entity class Book

---
+++ BookService_two/src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,17 @@
+package com.example.demo.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Book {
+  int BookId;
+  String BookName;
+  String author;
+}
